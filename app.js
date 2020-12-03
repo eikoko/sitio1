324cedbeb17cfd82100aa6c378d4403438dc63e8
eikoko <eikopedraza237@gmail.com>
2020-12-03 11:03:57 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const path=require('path');
+
+const app=express();
+app.set("view engine","ejs");
+app.use("/public", express.static(path.join(__dirname,'/public')));
+
+app.get('/',(req,res)=>{
+    res.render("index",{titulo:'Título del index'});
+});
+
+app.get('/about',(req,res)=>{
+    res.render("about",{titulo:'Título del about'});
+});
+
+app.get('/servicios',(req,res)=>{
+    res.render("servicios",{titulo:'Título de servicios'});
+});
+
+const port=process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log('Servidor listo en el puerto '+port);
+})
